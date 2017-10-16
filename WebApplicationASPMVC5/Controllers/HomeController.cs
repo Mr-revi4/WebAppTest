ad8b0e230ea35874a3b0ff47cda94500c11079a5
Mr-revi4 <mr_revi4@mail.ru>
2017-10-16 14:49:45 +0300

    Create first ASP.NET MVC5 application

---
+++ WebApplicationASPMVC5/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using WebApplicationASPMVC5.Models;
+
+namespace WebApplicationASPMVC5.Controllers
+{
+    public class HomeController : Controller
+    {
+        BookContext db = new BookContext(); // создаем контекст данных
+
+        public ActionResult Index()
+        {            
+            IEnumerable<Book> books = db.Books; // получаем из бд все объекты Book
+            ViewBag.Books = books; // передаем все объекты в динамическое свойство Books в ViewBag            
+            return View(); // возвращаем представление
+        }
+
+        [HttpGet]
+        public ActionResult Buy(int id)
+        {
+            ViewBag.BookId = id;
+            return View();
+        }
+        [HttpPost]
+        public string Buy(Purchase purchase)
+        {
+            purchase.Date = DateTime.Now;
+            db.Purchases.Add(purchase);
+            db.SaveChanges();
+            return "Спасибо," + purchase.Person + ", за покупку!";
+        }
+    }
+}
---
+++ WebApplicationASPMVC5/Global.asax.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Data.Entity;
+using WebApplicationASPMVC5.Models;
+
+namespace WebApplicationASPMVC5
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            Database.SetInitializer(new BookDbInitializer());
+
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
