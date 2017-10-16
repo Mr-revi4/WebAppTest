9cfacd7aa1de922d2cb0f1dbce1b2d4a3aedb7be
Mr-revi4 <mr_revi4@mail.ru>
2017-10-16 18:49:28 +0300

    Add  list of buyers

---
+++ WebApplicationASPMVC5/Controllers/ListController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using WebApplicationASPMVC5.Models;
+
+namespace WebApplicationASPMVC5.Controllers
+{
+    public class ListController : Controller
+    {
+        BookContext db = new BookContext();
+
+        public ActionResult Index()
+        {
+            IEnumerable<Purchase> purchases = db.Purchases;
+            ViewBag.Purchases = purchases;
+            return View();
+        }
+    }
+}
