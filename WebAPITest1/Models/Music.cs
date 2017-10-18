00260af4c626ad1de49c7c475a90c828966ce0a8
Mr-revi4 <mr_revi4@mail.ru>
2017-10-18 19:16:22 +0300

    Create new WebAPI application together with AngularJS 1.6

---
+++ WebAPITest1/Models/Music.cs
@@ -0,0 +1,12 @@
+﻿namespace WebAPITest1.Models
+{
+    public class Music
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public string Album { get; set; }
+        public int Year { get; set; }        
+        public string Genre { get; set; }
+    }
+}
---
+++ WebAPITest1/Models/MusicContext.cs
@@ -0,0 +1,9 @@
+﻿using System.Data.Entity;
+
+namespace WebAPITest1.Models
+{
+    public class MusicContext: DbContext
+    {
+        public DbSet<Music> Music { get; set; }
+    }
+}
